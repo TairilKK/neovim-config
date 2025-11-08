dd1b991db948aa6224104569059e652869dc56e0
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-11-08 12:30:29 +0100

    Add options and some refactor

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,1 +1,60 @@
-return { 'hrsh7th/nvim-cmp' }
+return { 
+    'hrsh7th/nvim-cmp',
+    'neovim/nvim-lspconfig',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-cmdline',
+    'hrsh7th/nvim-cmp',
+
+    'hrsh7th/cmp-vsnip',
+    'hrsh7th/vim-vsnip',
+    config = function()
+        local cmp = require("cmp")
+        local luasnip = require("luasnip")
+        require("luasnip.loaders.from_vscode").lazy_load() -- snippet'y z VS Code
+
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    luasnip.lsp_expand(args.body)
+                end,
+            },
+            mapping = cmp.mapping.preset.insert({
+                ["<C-Space>"] = cmp.mapping.complete(),        -- ręczne wywołanie
+                ["<CR>"]      = cmp.mapping.confirm({ select = true }),
+                ["<Tab>"]     = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                    elseif luasnip.expand_or_jumpable() then
+                        luasnip.expand_or_jump()
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+                ["<S-Tab>"]   = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    elseif luasnip.jumpable(-1) then
+                        luasnip.jump(-1)
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+            }),
+            sources = cmp.config.sources({
+                { name = "nvim_lsp", priority = 1000 },
+                { name = "luasnip",  priority = 750 },
+                { name = "buffer",   priority = 500 },
+                { name = "path",     priority = 250 },
+            }),
+            formatting = {
+                format = require("lspkind").cmp_format({
+                    mode = "symbol_text",
+                    maxwidth = 50,
+                    ellipsis_char = "...",
+                }),
+            },
+        })
+    end,
+}
